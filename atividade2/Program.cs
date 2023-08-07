22db0d581b5e64aaae55c100e787882fdf2271e0
Azevedo <pedrohxpa@hotmail.com>
2023-08-07 16:54:22 -0400

    fisrt Commit

---
+++ atividade2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        List<Aluno> list = new List<Aluno>();
+        
+        while (true)
+        {
+            Aluno a = new Aluno();
+
+            Console.Write(" \n 0- Se você quer encerrar \n 1- Se você quer cadastrar mais \n Digite: ");
+            int opcaoEscolhida = Convert.ToInt32(Console.ReadLine()); 
+            switch (opcaoEscolhida)
+            {
+                case 0:
+                    
+                    goto exit;
+                   
+
+                case 1:
+                    Console.Write("Digite o numero de matrícula: ");
+                    a.Matricula = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Digite o nome: ");
+                    a.Nome = Console.ReadLine();
+
+                    Console.Write("Digite o Cpf: ");
+                    a.Cpf = Console.ReadLine();
+
+                    Console.Write("Digite a data de nascimento: ");
+                    a.DataNascimento = DateOnly.Parse(Console.ReadLine());   
+                    
+                    list.Add(a);
+                    break;
+            }
+        }
+        exit:; ;
+
+        foreach (Aluno a in list)
+        {
+            Console.WriteLine($"{a.Nome}, seu número de matrícula é: {a.Matricula}, seu cpf é: {a.Cpf}, e sua data de nascimento: {a.DataNascimento} ");
+        }
+
+
+
+
+    }
+}
---
+++ atividade2/aluno.cs
@@ -0,0 +1,23 @@
+﻿public class Aluno
+{
+    public int Matricula { get; set; }
+
+    public string Nome { get; set; }
+
+    public string Cpf { get; set; }
+
+    public DateOnly DataNascimento { get; set; }
+
+    public Aluno()
+    {
+
+    }
+
+    public Aluno(int Matricula, string Nome, string Cpf, DateOnly DataNascimento)
+    {
+        this.Matricula = Matricula;
+        this.Nome = Nome;
+        this.Cpf = Cpf;
+        this.DataNascimento = DataNascimento;
+    }
+}
